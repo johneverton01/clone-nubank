9563047f51a37f3f314701df4c25cc05599fd2ce
John Everton <johneverton44@gmail.com>
2020-12-13 07:45:59 -0300

    create animation

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -4,9 +4,14 @@ import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import { Container, Code, Nav, NavItem, NavText, SingOutButton, SingOutButtonText } from './styles';
 
-export default function Menu() {
+export default function Menu({ translateY }) {
     return (
-        <Container>
+        <Container style={{
+            opacity: translateY.interpolate({
+                inputRange: [0, 150],
+                outputRange: [0, 1],
+            })
+        }}>
             <Code>
                 <QRCode
                     value="https://johncode.com.br"
